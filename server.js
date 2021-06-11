e6b357e9490292c2a526f33289958e46e58a6103
govindkumwat <kumawatgovind1997@gmail.com>
2021-06-11 22:32:11 +0530

    added server file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const jsonServer = require("json-server");
-const cors = require("cors");
+// const cors = require("cors");
 const path = require("path");
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, "events.json"));
 const middlewares = jsonServer.defaults();
 
-server.use(cors());
+// server.use(cors());
 server.use(jsonServer.bodyParser);
 server.use(middlewares);
 server.use(router);
